755e592a691037913e00a6f7bff316d7d7970281
Maico A Kley <maicokley@hotmail.com>
2020-01-16 23:42:46 -0300

    layout devradar

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './global.css'
 import './App.css'
 import './Sidebar.css'
+import './Main.css'
 
 // Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação.
 // Propriedade: Informações que um componente PAI passa para o componente FILHO
 // Estado: Informações mantidas pelo componente (Lembrar: Imutabilidade)
 
 function App() {  
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000
+      }
+    )
+
+  }, []);
+
   return (
     <div id="app">
       <aside>
@@ -35,11 +51,58 @@ function App() {
               <input name="longitude" id="longitude" required/>
             </div>
           </div>         
-            
+          
+          <button type="submit">Salvar</button>
         </form>
       </aside>
-      <main>
 
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/27367456?s=460&v=4" alt="Maico Alexandre Kley"/>
+              <div className="user-info">
+                <strong>Maico Alexandre Kley</strong>
+                <span>Node.js, ReactJS</span>
+              </div>
+            </header>
+            <p>Mais um programador aleatório...</p>
+            <a href="https://github.com/MaicoKley">Acessar perfil</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/27367456?s=460&v=4" alt="Maico Alexandre Kley"/>
+              <div className="user-info">
+                <strong>Maico Alexandre Kley</strong>
+                <span>Node.js, ReactJS</span>
+              </div>
+            </header>
+            <p>Mais um programador aleatório...Mais um programador aleatório...Mais um programador aleatório...Mais um programador aleatório...Mais um programador aleatório...Mais um programador aleatório...Mais um programador aleatório...</p>
+            <a href="https://github.com/MaicoKley">Acessar perfil</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/27367456?s=460&v=4" alt="Maico Alexandre Kley"/>
+              <div className="user-info">
+                <strong>Maico Alexandre Kley</strong>
+                <span>Node.js, ReactJS</span>
+              </div>
+            </header>
+            <p>Mais um programador aleatório...</p>
+            <a href="https://github.com/MaicoKley">Acessar perfil</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/27367456?s=460&v=4" alt="Maico Alexandre Kley"/>
+              <div className="user-info">
+                <strong>Maico Alexandre Kley</strong>
+                <span>Node.js, ReactJS</span>
+              </div>
+            </header>
+            <p>Mais um programador aleatório...</p>
+            <a href="https://github.com/MaicoKley">Acessar perfil</a>
+          </li>
+        </ul>
       </main>
     </div>
   );
