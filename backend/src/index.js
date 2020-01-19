88e62b82a839cdcf5f300da7048cb705bfd5d913
Maico A Kley <maicokley@hotmail.com>
2020-01-19 19:20:13 -0300

    implementacao websockets

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
+const { setupWebSocket } = require('./websocket')
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-2mg7k.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -26,4 +31,4 @@ app.use(routes);
 
 // MongoDB (Não-relacional)
 
-app.listen(3333);
+server.listen(3333);
