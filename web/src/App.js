0d426d81f12f2a7f132fd76acc6f84c7b5d526c8
Maico A Kley <maicokley@hotmail.com>
2020-01-18 22:43:41 -0300

    front end devradar final

--- web/src/App.js
+++ web/src/App.js
@@ -1,107 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api'
 
 import './global.css'
 import './App.css'
 import './Sidebar.css'
 import './Main.css'
 
+import DevItem from './components/DevItem';
+import DevForm from './components/DevForm'
+
 // Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação.
 // Propriedade: Informações que um componente PAI passa para o componente FILHO
 // Estado: Informações mantidas pelo componente (Lembrar: Imutabilidade)
 
-function App() {  
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        console.log(position);
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000
-      }
-    )
+function App() {
+  const [devs, setDevs] = useState([]); 
 
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
   }, []);
+
+  async function handleAddDev(data) {   
+    const response = await api.post('/devs', data)
+
+    setDevs([...devs, response.data]);
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required/>
-          </div>
-          
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required/>
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required/>
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required/>
-            </div>
-          </div>         
-          
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/27367456?s=460&v=4" alt="Maico Alexandre Kley"/>
-              <div className="user-info">
-                <strong>Maico Alexandre Kley</strong>
-                <span>Node.js, ReactJS</span>
-              </div>
-            </header>
-            <p>Mais um programador aleatório...</p>
-            <a href="https://github.com/MaicoKley">Acessar perfil</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/27367456?s=460&v=4" alt="Maico Alexandre Kley"/>
-              <div className="user-info">
-                <strong>Maico Alexandre Kley</strong>
-                <span>Node.js, ReactJS</span>
-              </div>
-            </header>
-            <p>Mais um programador aleatório...Mais um programador aleatório...Mais um programador aleatório...Mais um programador aleatório...Mais um programador aleatório...Mais um programador aleatório...Mais um programador aleatório...</p>
-            <a href="https://github.com/MaicoKley">Acessar perfil</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/27367456?s=460&v=4" alt="Maico Alexandre Kley"/>
-              <div className="user-info">
-                <strong>Maico Alexandre Kley</strong>
-                <span>Node.js, ReactJS</span>
-              </div>
-            </header>
-            <p>Mais um programador aleatório...</p>
-            <a href="https://github.com/MaicoKley">Acessar perfil</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/27367456?s=460&v=4" alt="Maico Alexandre Kley"/>
-              <div className="user-info">
-                <strong>Maico Alexandre Kley</strong>
-                <span>Node.js, ReactJS</span>
-              </div>
-            </header>
-            <p>Mais um programador aleatório...</p>
-            <a href="https://github.com/MaicoKley">Acessar perfil</a>
-          </li>
+          {devs.map(dev => (
+              <DevItem key={dev._id} dev={dev} />
+          ))}                  
         </ul>
       </main>
     </div>
